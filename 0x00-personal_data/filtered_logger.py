b575635658a65d7b5b9f622e79fda740f8c39818
sezfabian <cheruiyotfabian@gmail.com>
2023-08-31 23:59:47 +0300

    pasword encryption

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,16 +1,23 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3i
 """
 Filtered Logger Module
 """
+from typing import List
 import re
+import logging
+import os
+import mysql.connector
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: list[str], redaction: str,
                  message: str, separator: str) -> str:
     """Returns the log message obfuscated"""
     for field in fields:
-        message = re.sub(field + "=.*?" + separator,
-                         field + "=" + redaction + separator, message)
+         for field in fields:
+        message = re.sub(rf"{field}=(.*?)\{separator}",
+                         f'{field}={redaction}{separator}', message)
     return message
 
 

3e149b45f656dca5096bf48323a0ca154d736665
sezfabian <cheruiyotfabian@gmail.com>
2023-09-11 13:41:47 +0300

    routes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,10 +8,16 @@ class Auth:
     """Class for authentication."""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Check if user is authenticated."""
-        if path in excluded_paths:
-            return False
-        else:
+        if excluded_paths is None:
             return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if path in excluded_paths or (path + '/') in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Authorization header."""

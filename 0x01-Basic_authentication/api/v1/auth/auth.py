1f89e9b6d111529121b6751f5829f323f2004545
sezfabian <cheruiyotfabian@gmail.com>
2023-09-11 13:58:04 +0300

    routes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,7 @@ class Auth:
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
 
-        if path in excluded_paths or (path + '/') in excluded_paths:
+        if path in excluded_paths or f"{path}'/'" in excluded_paths:
             return False
 
         return True

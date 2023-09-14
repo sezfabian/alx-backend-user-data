e9b8b1d51a2ea8e7850c1947af8ee3635bcfbe6e
sezfabian <cheruiyotfabian@gmail.com>
2023-09-15 01:46:44 +0300

    log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Flask app module
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
@@ -49,6 +49,21 @@ def login():
         return response
 
     abort(401)
+
+
+@app.route("/sessions", methods=["DELETE"])
+def logout():
+    """Destroy a user’s session
+    """
+    session_id = request.cookies.get("session_id")
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user is None:
+        abort(403)
+
+    AUTH.destroy_session(user.id)
+    redirect("/")
 
 
 if __name__ == "__main__":

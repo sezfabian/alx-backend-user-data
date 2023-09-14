b44fd397980c4fc2a599a8606fda4d2c93f98634
sezfabian <cheruiyotfabian@gmail.com>
2023-09-15 02:10:34 +0300

    reset pass token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -99,3 +99,22 @@ class Auth:
             return None
 
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Find the user corresponding to the email.
+        If the user does not exist,
+        raise a ValueError exception.
+        If it exists, generate a UUID and
+        update the user’s reset_token database field.
+        Finally, Return the token.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+
+        except NoResultFound:
+            raise ValueError
+
